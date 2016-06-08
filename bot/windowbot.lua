0944e3b4d47836f759ac9e3c3b5150933d738e68
mohammad <Sojerbot@users.noreply.github.com>
2016-06-09 04:03:33 +0430

    Update and rename seedbot.lua to windowbot.lua

--- bot/windowbot.lua
+++ bot/windowbot.lua
@@ -224,32 +224,23 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "remsg",
+    "time"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {116251654,116773423},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[👑SUDOS USERNAME:@SUDO_window and @Nimaking13 
 
-https://github.com/SEEDTEAM/TeleSeed
-
-Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
-
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
-
-Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+ــــــــــــــــــــــــــــــــــــــــــــــ
+🤖BOT:https://telegram.me/MEGA_WINDOW
+ـــــــــــــــــــــــــــــــــــــــــــــ
+🔴CHANNEL:@Channel_window
+ــــــــــــــــــــــــــــــــــــــــــــــ
+🤖📩ROBOTPAYAMRESAN:@window_report_BOT
+ــــــــــــــــــــــــــــــــــــــــــــــ
+🎫☯LINK SUPPORT: ---
 ]],
     help_text_realm = [[
 Realm Commands:
